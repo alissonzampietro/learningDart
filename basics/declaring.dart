fe784cd44ddc1be38936797f6fa30891f228d129
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-02-13 23:18:01 +0000

    feat(basics): variables

---
+++ basics/declaring.dart
@@ -0,0 +1,11 @@
+void main() {
+    // Below is setting a variable
+    var name = 'Alisson';
+    print(name);
+    name = 'Nick';
+    print(name);
+
+    // Setting constant
+    const country = 'Portugal';
+    print(country);
+}
