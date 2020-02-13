91610d1a1a465d8d5d962a89be2b5fbf17ec2684
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-02-13 23:42:39 +0000

    feat(basics): sum

--- basics/operators.dart
+++ basics/operators.dart
@@ -1,9 +1,21 @@
 void main() {
     /* 
+     * Number to make operations
+     */
+    int a = 20;
+    int b = 7;
+
+    /* 
      * Concatening values (two ways)
      */
     var name = 'Alisson';
     print("Olá meu nome é "+ name);
     print("Olá meu nome é $name");
     print("Olá meu nome é ${name}");
+
+    /* 
+     * Sum numbers
+     */
+    var sum = a + b;
+    print(sum);
 }
