84be9ae7ec13801badc8294588419ac8e4bca887
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-02-14 00:19:43 +0000

    feat(basics): loops

---
+++ basics/loops.dart
@@ -0,0 +1,19 @@
+void main() {
+  int defaultNumber = 0;
+
+  /* 
+   * For
+   */
+  for(int i = defaultNumber; i < 10; i++) {
+    print('Eu já me repeti $i vezes');
+  }
+
+  /* 
+   * While
+   */
+  int temp = 0;
+  while(temp <= defaultNumber) {
+    print('Eu estou no número $temp e faltam ${defaultNumber - temp} para chegar no $defaultNumber');
+    temp++;
+  }
+}
